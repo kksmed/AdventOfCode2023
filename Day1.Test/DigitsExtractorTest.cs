a32b7104439a5818522a8073ebae83d956ee7841
Kristoffer Smed <mail@kristoffersmed.dk>
2023-12-04 10:31:08 +0100

    Day 1 part 2 - failed first attempt.

--- Day1.Test/DigitsExtractorTest.cs
+++ Day1.Test/DigitsExtractorTest.cs
@@ -5,7 +5,15 @@ class DigitsExtractorTest
   [TestCase("1abc2", 12),
    TestCase("pqr3stu8vwx", 38),
    TestCase("a1b2c3d4e5f", 15),
-   TestCase("treb7uchet)]", 77)]
+   TestCase("treb7uchet)]", 77),
+   TestCase("two1nine)]", 29),
+   TestCase("eightwothree)]", 83),
+   TestCase("abcone2threexyz)]", 13),
+   TestCase("xtwone3four)]", 24),
+   TestCase("4nineeightseven2)]", 42),
+   TestCase("zoneight234)]", 14),
+   TestCase("7pqrstsixteen)]", 76),
+  ]
   public void GetCalibrationValue(string str, int expected)
   {
     var calibrationValue = DigitsExtractor.GetCalibrationValue(str);
--- Day1/DigitsExtractor.cs
+++ Day1/DigitsExtractor.cs
@@ -2,9 +2,27 @@ namespace Day1;
 
 public static class DigitsExtractor
 {
+  static (string Word, string Number)[] digitWords =
+    {
+      ("one", "1one"),
+      ("two", "2two"),
+      ("three", "three"),
+      ("four", "4four"),
+      ("five", "5five"),
+      ("six", "6six"),
+      ("seven", "7seven"),
+      ("eight", "8eight"),
+      ("nine", "9nine")
+    };
+
+  internal static string ReplaceDigitWords(string str) => digitWords.Aggregate(
+    str,
+    (current, p) => current.Replace(p.Word, p.Number.ToString()));
+
   public static int GetCalibrationValue(string str)
   {
-    var digits = str.Where(char.IsDigit).ToList();
+    var strWithDigits = ReplaceDigitWords(str);
+    var digits = strWithDigits.Where(char.IsDigit).ToList();
     if (digits.Count < 1) throw new ArgumentException("No digits found.", nameof(str));
 
     return int.Parse(new string(new[] { digits.First(), digits.Last() }));
