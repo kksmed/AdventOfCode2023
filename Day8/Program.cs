6d8c3224fe813e5c8ef41ae2540bfbc3dc97b140
Kristoffer Smed <mail@kristoffersmed.dk>
2023-12-18 23:14:18 +0100

    Day 8 - part 1 correct.

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -13,13 +13,11 @@ ZZZ = (ZZZ, ZZZ)";
 const string start = "AAA";
 const string end = "ZZZ";
 
-var (testDistance, testP, testMap) = FindShortestPath(Parser.ParseMap(test.Split(Environment.NewLine)));
+var testDistance = FindDistance(Parser.ParseMap(test.Split(Environment.NewLine)));
 Console.WriteLine($"Test: {testDistance}");
-PrintPath(testP, testMap);
 
-var (part1Distance, part1P, part1Map) = FindShortestPath(Parser.ParseMap(File.ReadAllLines("input8.txt")));
-Console.WriteLine($"Test: {part1Distance}");
-PrintPath(part1P, part1Map);
+var part1Distance = FindDistance(Parser.ParseMap(File.ReadAllLines("input8.txt")));
+Console.WriteLine($"Part 1: {part1Distance}");
 
 return;
 
@@ -41,43 +39,27 @@ void PrintPath(Dictionary<string, string> prev, Dictionary<string, Node> diction
 
 Dictionary<string, Node> CreateMap(IEnumerable<Node> nodes) => nodes.ToDictionary(x => x.Id, x => x);
 
-(int, Dictionary<string, string> previous, Dictionary<string, Node> map) FindShortestPath(
-  (string Directions, IEnumerable<Node> Nodes) data)
+int FindDistance((string Directions, IEnumerable<Node> Nodes) data)
 {
   var map = CreateMap(data.Nodes);
   var distances = map.Keys.ToDictionary(x => x, _ => int.MaxValue);
   distances[start] = 0;
-  var previous = map.Keys.ToDictionary(x => x, _ => (string?)null);
-  var visited = map.Keys.ToDictionary(x => x, _ => false);
 
-  var queue = new Queue<string>();
-  queue.Enqueue(start);
-  while (queue.Count > 0)
+  var queue = new Queue<char>(data.Directions);
+  var current = map[start];
+  var distance = 0;
+  do
   {
-    var current = map[queue.Dequeue()];
-    visited[current.Id] = true;
+    var turn = queue.Dequeue();
+    Console.Write($"{current.Id} -({turn})-> ");
+    current = map[turn == 'L' ? current.Left : current.Right];
 
-    VisitTurn(queue, previous, visited, distances, current.Id, current.Left);
-    VisitTurn(queue, previous, visited, distances, current.Id, current.Right);
-  }
+    distance++;
+    queue.Enqueue(turn);
+  } while (current.Id != end);
+  Console.WriteLine($"-> {current.Id}.");
 
-  return (distances[end], previous, map)!;
+  return distance;
 }
 
-void VisitTurn(
-  Queue<string> queue,
-  Dictionary<string, string?> previous,
-  Dictionary<string, bool> visited,
-  Dictionary<string, int> distances,
-  string current,
-  string next)
-{
-  if (visited[next] || queue.Contains(next)) return;
 
-  queue.Enqueue(next);
-  var d = distances[current] + 1;
-  if (distances[next] <= d) return;
-
-  distances[next] = d;
-  previous[next] = current;
-}
