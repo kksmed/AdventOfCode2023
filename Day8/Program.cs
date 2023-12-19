ebf91e2f0fa35b26248a1dab1915fec3ba7b6ab8
Kristoffer Smed <mail@kristoffersmed.dk>
2023-12-19 07:34:18 +0100

    Day 8 - part 2 (never stopping?).

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Day8;
 
-const string test = @"RL
+const string test1 = @"RL
 
 AAA = (BBB, CCC)
 BBB = (DDD, EEE)
@@ -12,30 +12,33 @@ ZZZ = (ZZZ, ZZZ)";
 
 const string start = "AAA";
 const string end = "ZZZ";
+const char startSurfix = 'A';
+const char endSurfix = 'Z';
 
-var testDistance = FindDistance(Parser.ParseMap(test.Split(Environment.NewLine)));
+var testDistance = FindDistance(Parser.ParseMap(test1.Split(Environment.NewLine)));
 Console.WriteLine($"Test: {testDistance}");
 
 var part1Distance = FindDistance(Parser.ParseMap(File.ReadAllLines("input8.txt")));
 Console.WriteLine($"Part 1: {part1Distance}");
 
-return;
+var test2 = @"LR
 
-void PrintPath(Dictionary<string, string> prev, Dictionary<string, Node> dictionary)
-{
-  var directions = new Stack<char>();
-  var current = end;
-  while (current != start)
-  {
-    Console.Write($"{current} <- ");
-    var to = current;
-    current = prev[current];
-    var node = dictionary[current];
-    directions.Push(node.Left == to ? 'L' : 'R');
-  }
-  Console.WriteLine($"{start}.");
-  Console.WriteLine(directions.ToArray());
-}
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+
+var testDistance2 = FindGhostDistance(Parser.ParseMap(test2.Split(Environment.NewLine)));
+Console.WriteLine($"Test2: {testDistance2}");
+
+var part2Distance = FindGhostDistance(Parser.ParseMap(File.ReadAllLines("input8.txt")));
+Console.WriteLine($"Part 2: {part2Distance}");
+
+return;
 
 Dictionary<string, Node> CreateMap(IEnumerable<Node> nodes) => nodes.ToDictionary(x => x.Id, x => x);
 
@@ -62,4 +65,26 @@ int FindDistance((string Directions, IEnumerable<Node> Nodes) data)
   return distance;
 }
 
+int FindGhostDistance((string Directions, IEnumerable<Node> Nodes) data)
+{
+  var map = CreateMap(data.Nodes);
+  var currents = map.Where(x => x.Key.EndsWith(startSurfix)).Select(x => x.Value).ToArray();
+  var distance = 0;
+  var t = 0;
+  while (!currents.All(x => x.Id.EndsWith(endSurfix)))
+  {
+    var turn = data.Directions[t];
+    for (var i = 0; i < currents.Length; i++)
+    {
+      var current = currents[i];
+      currents[i] = map[turn == 'L' ? current.Left : current.Right];
+    }
+
+    distance++;
+    t++;
+    if (t == data.Directions.Length) t = 0;
+  }
+
+  return distance;
+}
 
