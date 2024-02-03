80e5706d26bb34d07285afa1a5827666b40609bf
Kristoffer Smed <mail@kristoffersmed.dk>
2024-02-03 21:08:19 +0100

    Day 9 - part 1.

---
+++ Day9/Program.cs
@@ -0,0 +1,31 @@
+﻿var testInput = """
+                0 3 6 9 12 15
+                1 3 6 10 15 21
+                10 13 16 21 30 45
+                """;
+
+var test1 = Part1Sum(testInput.Split(Environment.NewLine));
+Console.WriteLine($"Test 1: {test1}");
+
+var part1 = Part1Sum(File.ReadAllLines("input9.txt"));
+Console.WriteLine($"Part 1: {part1}");
+
+return;
+
+int Part1Sum(IEnumerable<string> lines)
+{
+  return lines
+    .Select(Parse)
+    .Select(PredictNext)
+    .Sum();
+
+  List<int> Parse(string line) => line.Split(' ').Select(int.Parse).ToList();
+
+  int PredictNext(ICollection<int> numbers)
+  {
+    var diffs = numbers.SkipLast(1).Zip(numbers.Skip(1)).Select(x => x.Second - x.First).ToList();
+    if (diffs.All(x => x == 0)) return numbers.Last();
+
+    return numbers.Last() + PredictNext(diffs);
+  }
+}
