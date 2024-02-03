c3ac676cfd97dbe4393338f2ac0699fd7957f778
Kristoffer Smed <mail@kristoffersmed.dk>
2024-02-03 21:14:58 +0100

    Day 9 - part 2.

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -10,7 +10,18 @@ Console.WriteLine($"Test 1: {test1}");
 var part1 = Part1Sum(File.ReadAllLines("input9.txt"));
 Console.WriteLine($"Part 1: {part1}");
 
+var test2 = Part2Sum(testInput.Split(Environment.NewLine));
+Console.WriteLine($"Test 2: {test2}");
+
+var part2 = Part2Sum(File.ReadAllLines("input9.txt"));
+Console.WriteLine($"Part 2: {part2}");
+
 return;
+
+List<int> Parse(string line) => line.Split(' ').Select(int.Parse).ToList();
+
+List<int> FindDiffs(ICollection<int> collection) =>
+  collection.SkipLast(1).Zip(collection.Skip(1)).Select(x => x.Second - x.First).ToList();
 
 int Part1Sum(IEnumerable<string> lines)
 {
@@ -19,13 +30,29 @@ int Part1Sum(IEnumerable<string> lines)
     .Select(PredictNext)
     .Sum();
 
-  List<int> Parse(string line) => line.Split(' ').Select(int.Parse).ToList();
 
   int PredictNext(ICollection<int> numbers)
   {
-    var diffs = numbers.SkipLast(1).Zip(numbers.Skip(1)).Select(x => x.Second - x.First).ToList();
+    var diffs = FindDiffs(numbers);
     if (diffs.All(x => x == 0)) return numbers.Last();
 
     return numbers.Last() + PredictNext(diffs);
+  }
+}
+
+int Part2Sum(IEnumerable<string> lines)
+{
+  return lines
+    .Select(Parse)
+    .Select(PredictPrevious)
+    .Sum();
+
+
+  int PredictPrevious(ICollection<int> numbers)
+  {
+    var diffs = FindDiffs(numbers);
+    if (diffs.All(x => x == 0)) return numbers.First();
+
+    return numbers.First() - PredictPrevious(diffs);
   }
 }
