37ec61d2209bc9df8208200ba88265772fe14d2f
Kristoffer Smed <mail@kristoffersmed.dk>
2024-02-05 21:40:14 +0100

    Day 10 - part 2.

--- Day10/Map.cs
+++ Day10/Map.cs
@@ -1,8 +1,11 @@
 using System.Diagnostics.CodeAnalysis;
 
+namespace Day10;
+
 class Map
 {
   readonly string[] lines;
+  readonly HashSet<Position> loop = new();
   readonly int maxY;
   readonly int maxX;
 
@@ -13,10 +16,82 @@ class Map
     maxX = lines[0].Length - 1;
   }
 
+  public int GetEnclosedTiles()
+  {
+    var start = FindStart();
+    SetLoop(start);
+    var (end1, end2) = FindConnections(start);
+    var s = DetermineStart(end1.Direction, end2.Direction);
+    lines[start.Y] = lines[start.Y].Replace('S', s);
+    SetLoop(end1.Position);
+    SetLoop(end2.Position);
+    while (true)
+    {
+      end1 = GoNext(end1);
+      if (end1.Position == end2.Position) break;
+      SetLoop(end1.Position);
+
+      end2 = GoNext(end2);
+      if (end1.Position == end2.Position) break;
+      SetLoop(end2.Position);
+    }
+
+    return FindEnclosed();
+  }
+
+  int FindEnclosed()
+  {
+    var enclosed = 0;
+    for (var x = 0; x <= maxX; x++)
+    {
+      var isEnclosed = false;
+      bool? fromLeft = null;
+      for (var y = 0; y <= maxY; y++)
+      {
+        if (loop.Contains(new Position(x, y)))
+        {
+          switch (this[x, y])
+          {
+            case '-':
+              isEnclosed = !isEnclosed;
+              continue;
+            case '7':
+              fromLeft = true;
+              continue;
+            case 'F':
+              fromLeft = false;
+              continue;
+            case 'L':
+              if (!fromLeft.HasValue) throw new InvalidOperationException($"{nameof(fromLeft)} is not set!");
+              if (fromLeft.Value)
+                isEnclosed = !isEnclosed;
+              fromLeft = null;
+              continue;
+            case 'J':
+              if (!fromLeft.HasValue) throw new InvalidOperationException($"{nameof(fromLeft)} is not set!");
+              if (!fromLeft.Value)
+                isEnclosed = !isEnclosed;
+              fromLeft = null;
+              continue;
+            case '|':
+              continue;
+            default: throw new InvalidOperationException($"Unexpected form '{this[x, y]}' at ({x}, {y})");
+          }
+        }
+
+        if (isEnclosed) enclosed++;
+      }
+    }
+
+    return enclosed;
+  }
+
+  void SetLoop(Position pos) => loop.Add(pos);
+
   public int GetFarthestDistance()
   {
-    var s = FindStart();
-    var (end1, end2) = FindConnections(s);
+    var start = FindStart();
+    var (end1, end2) = FindConnections(start);
     var distance = 1;
     while (true)
     {
@@ -62,7 +137,8 @@ class Map
         _ => throw new ArgumentOutOfRangeException(nameof(pipe.Direction), pipe.Direction, null)
       };
 
-  Char this[Position pos] => lines[pos.Y][pos.X];
+  char this[int x, int y] => lines[y][x];
+  char this[Position pos] => this[pos.X, pos.Y];
 
   Pipe Go(Pipe current, Direction dir)
   {
@@ -112,6 +188,39 @@ class Map
 
     throw new ArgumentException("No start position", nameof(lines));
   }
+
+  static char DetermineStart(Direction d1, Direction d2) => d1 switch
+    {
+      Direction.Up => d2 switch
+        {
+          Direction.Down => '|',
+          Direction.Left => 'J',
+          Direction.Right => 'L',
+          _ => throw new InvalidOperationException($"Invalid start pipe: {d1} & {d2}")
+        },
+      Direction.Down => d2 switch
+        {
+          Direction.Up => '|',
+          Direction.Left => '7',
+          Direction.Right => 'F',
+          _ => throw new InvalidOperationException($"Invalid start pipe: {d1} & {d2}")
+        },
+      Direction.Left => d2 switch
+        {
+          Direction.Down => '7',
+          Direction.Up => 'J',
+          Direction.Right => '-',
+          _ => throw new InvalidOperationException($"Invalid start pipe: {d1} & {d2}")
+        },
+      Direction.Right => d2 switch
+        {
+          Direction.Down => 'F',
+          Direction.Up => 'L',
+          Direction.Left => '-',
+          _ => throw new InvalidOperationException($"Invalid start pipe: {d1} & {d2}")
+        },
+      _ => throw new ArgumentOutOfRangeException(nameof(d1), d1, null)
+    };
 
   (Pipe, Pipe) FindConnections(Position pos)
   {
--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,4 +1,6 @@
-﻿var testInput1 = """
+﻿using Day10;
+
+var testInput1 = """
                  .....
                  .S-7.
                  .|.|.
@@ -6,7 +8,7 @@
                  .....
                  """;
 var test1 = Part1(testInput1.Split(Environment.NewLine));
-Console.WriteLine($"Test1: {test1}");
+Console.WriteLine($"{nameof(test1)}: {test1}");
 
 var testInput2 = """
                  ..F7.
@@ -16,12 +18,78 @@ var testInput2 = """
                  LJ...
                  """;
 var test2 = Part1(testInput2.Split(Environment.NewLine));
-Console.WriteLine($"Test2: {test2}");
+Console.WriteLine($"{nameof(test2)}: {test2}");
 
 var part1 = Part1(File.ReadAllLines("input10.txt"));
-Console.WriteLine($"Part1: {part1}");
+Console.WriteLine($"{nameof(part1)}: {part1}");
 
+var testInput3 = """
+             ...........
+             .S-------7.
+             .|F-----7|.
+             .||.....||.
+             .||.....||.
+             .|L-7.F-J|.
+             .|..|.|..|.
+             .L--J.L--J.
+             ...........
+             """;
+
+var test3 = Part2(testInput3.Split(Environment.NewLine));
+Console.WriteLine($"{nameof(test3)}: {test3}");
+
+var testInput3B = """
+           ..........
+           .S------7.
+           .|F----7|.
+           .||OOOO||.
+           .||OOOO||.
+           .|L-7F-J|.
+           .|II||II|.
+           .L--JL--J.
+           ..........
+           """;
+
+var test3B = Part2(testInput3B.Split(Environment.NewLine));
+Console.WriteLine($"{nameof(test3B)}: {test3B}");
+
+var testInput4 = """
+                 .F----7F7F7F7F-7....
+                 .|F--7||||||||FJ....
+                 .||.FJ||||||||L7....
+                 FJL7L7LJLJ||LJ.L-7..
+                 L--J.L7...LJS7F-7L7.
+                 ....F-J..F7FJ|L7L7L7
+                 ....L7.F7||L7|.L7L7|
+                 .....|FJLJ|FJ|F7|.LJ
+                 ....FJL-7.||.||||...
+                 ....L---J.LJ.LJLJ...
+                 """;
+
+var test4 = Part2(testInput4.Split(Environment.NewLine));
+Console.WriteLine($"{nameof(test4)}: {test4}");
+
+var testInput5 = """
+                 FF7FSF7F7F7F7F7F---7
+                 L|LJ||||||||||||F--J
+                 FL-7LJLJ||||||LJL-77
+                 F--JF--7||LJLJ7F7FJ-
+                 L---JF-JLJ.||-FJLJJ7
+                 |F|F-JF---7F7-L7L|7|
+                 |FFJF7L7F-JF7|JL---7
+                 7-L-JL7||F7|L7F-7F7|
+                 L.L7LFJ|||||FJL7||LJ
+                 L7JLJL-JLJLJL--JLJ.L
+                 """;
+
+var test5 = Part2(testInput5.Split(Environment.NewLine));
+Console.WriteLine($"{nameof(test5)}: {test5}");
+
+var part2 = Part2(File.ReadAllLines("input10.txt"));
+Console.WriteLine($"Part4: {part2}");
 
 return;
 
 int Part1(string[] lines) => new Map(lines).GetFarthestDistance();
+
+int Part2(string[] lines) => new Map(lines).GetEnclosedTiles();
