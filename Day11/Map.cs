0794ec3be6786a07abe7e3dcd175a99090afee90
Kristoffer Smed <mail@kristoffersmed.dk>
2024-02-07 12:28:56 +0100

    Day 11 - part 2.

--- Day11/Map.cs
+++ Day11/Map.cs
@@ -2,37 +2,27 @@ namespace Day11;
 
 public class Map
 {
-  string[] lines;
+  readonly string[] lines;
+  readonly long expansion;
+  readonly List<int> expandedColumns = new();
+  readonly List<int> expandedRows = new();
 
-  public Map(string[] lines)
+  public Map(string[] lines, long expansion)
   {
     this.lines = lines;
+    this.expansion = expansion-1;
   }
 
   public void Expand()
   {
-    List<int> columnsToExpand = new(Enumerable.Range(0, lines[0].Length));
-    List<int> rowsToExpand = new();
+    expandedColumns.AddRange(Enumerable.Range(0, lines[0].Length));
     for (var y = 0; y < lines.Length; y++)
     {
       var line = lines[y];
       var space = line.Select((x, i) => (Char: x, X: i)).Where(x => x.Char == '.').Select(x => x.X).ToList();
-      if (space.Count == line.Length) rowsToExpand.Add(y);
-      columnsToExpand = columnsToExpand.Intersect(space).ToList();
+      if (space.Count == line.Length) expandedRows.Add(y);
+      expandedColumns.RemoveAll(x => !space.Contains(x));
     }
-
-    var newLines = lines.Select(x => x.ToList()).ToList();
-    foreach (var rowToAdd in rowsToExpand.OrderDescending())
-    {
-      newLines.Insert(rowToAdd, newLines[rowToAdd]);
-    }
-
-    columnsToExpand = columnsToExpand.OrderDescending().ToList();
-    foreach (var row in newLines)
-    foreach (var columnToAdd in columnsToExpand)
-      row.Insert(columnToAdd, '.');
-
-    lines = newLines.Select(x => new string(x.ToArray())).ToArray();
   }
 
   public IEnumerable<Position> FindGalaxies()
@@ -48,6 +38,16 @@ public class Map
         yield return new Position(x, y);
       }
     }
+  }
+
+  public long Distance(Position g1, Position g2)
+  {
+    var minX = Math.Min(g1.X, g2.X);
+    var maxX = Math.Max(g1.X, g2.X);
+    var minY = Math.Min(g1.Y, g2.Y);
+    var maxY = Math.Max(g1.Y, g2.Y);
+    return maxX - minX + maxY - minY
+      + (expandedRows.Count(y => minY < y && y < maxY) + expandedColumns.Count(x => minX < x && x < maxX)) * expansion;
   }
 }
 
--- Day11/Program.cs
+++ Day11/Program.cs
@@ -19,24 +19,41 @@ Console.WriteLine($"{nameof(test1)}: {test1}");
 var part1 = Part1(File.ReadAllLines("input11.txt"));
 Console.WriteLine($"{nameof(part1)}: {part1}");
 
+var test10 = Part2(testInput.Split(Environment.NewLine), 10);
+Console.WriteLine($"{nameof(test10)}: {test10}");
+
+var test100 = Part2(testInput.Split(Environment.NewLine), 100);
+Console.WriteLine($"{nameof(test100)}: {test100}");
+
+var part2 = Part2(File.ReadAllLines("input11.txt"), 1000000);
+Console.WriteLine($"{nameof(part2)}: {part2}");
 
 return;
 
-int Part1(string[] lines)
+long Part1(string[] lines)
 {
-  var map = new Map(lines);
+  var map = new Map(lines, 2);
   map.Expand();
 
   var galaxies = map.FindGalaxies().ToList();
-  // Console.WriteLine(string.Join(Environment.NewLine, galaxies));
 
   return AllPairs(galaxies)
-    .Select(x => Distance(x.Item1, x.Item2))
+    .Select(x => map.Distance(x.Item1, x.Item2))
+    .Sum();
+}
+
+long Part2(string[] lines, long expansion)
+{
+  var map = new Map(lines, expansion);
+  map.Expand();
+
+  var galaxies = map.FindGalaxies().ToList();
+
+  return AllPairs(galaxies)
+    .Select(x => map.Distance(x.Item1, x.Item2))
     .Sum();
 }
 
 IEnumerable<(T, T)> AllPairs<T>(ICollection<T> elements) =>
   elements.SelectMany((_, i) => elements.Skip(i + 1), (x, y) => (x, y));
 
-int Distance(Position g1, Position g2) =>
-  Math.Abs(g1.X - g2.X) + Math.Abs(g1.Y - g2.Y); 
